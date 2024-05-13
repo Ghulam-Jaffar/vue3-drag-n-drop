945cf0a90ed7727c45d47c8db547fb36acd9dec3
Ghulam Jaffar <herojaf12@gmail.com>
2024-05-14 02:29:00 +0500

    chore: changed to deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
-import vue from "@vitejs/plugin-vue";
-import {resolve} from "path";
-import { defineConfig } from "vite";
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: false,
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es"],
-      name: "Vue3DragNDrop",
-      fileName: (format) => `vue3-drag-n-drop.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          Vue: "vue",
-        },
-      },
-    },
-  },
-  plugins: [vue()],
-});
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
